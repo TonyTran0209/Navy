1f40c1b84d354499eccfe0832af2fc163fddd51d
Tony Tran <phi.tran.0209@gmail.com>
2018-03-02 02:11:53 +0700

    BLOGS: breadcrumbs, Read More, Related Blogs By Tags, Same In Category, Tags, Latest Blog, Most Viewed, Blogs

--- catalog/language/en-gb/extension/module/pavblog.php
+++ catalog/language/en-gb/extension/module/pavblog.php
@@ -4,7 +4,7 @@ $_['text_published_in'] = 'Published In: ';
 $_['text_created_date'] = 'Created Date: ';
 $_['text_hits'] = 'Hits: ';
 $_['text_comment_count'] = 'Comment: ';
-$_['text_readmore'] = 'Read More';
+$_['text_readmore'] = 'Xem Thêm';
 $_['text_leave_a_comment'] = 'Leave A Comment';
 
 
@@ -12,12 +12,12 @@ $_['text_leave_a_comment'] = 'Leave A Comment';
  $_['error_email'] = 'Email is not correct';
  $_['error_comment'] = 'Email is not correct';
  $_['error_user'] = 'Fullname is not correct';
- $_['text_in_related_by_tag'] = 'Related Blogs By Tags'; 
+ $_['text_in_related_by_tag'] = 'Tin gắn thẻ liên quan'; 
 /**
  *
  */
  $_['text_children'] = 'Children';
- $_['text_in_same_category'] = 'Same In Category';
+ $_['text_in_same_category'] = 'Tin Cùng Danh Mục';
  $_['text_list_comments'] = 'Comments';
  $_['text_created'] = 'Created By';
  $_['text_postedby'] = 'Posted By';
@@ -26,7 +26,7 @@ $_['text_leave_a_comment'] = 'Leave A Comment';
  $_['entry_email'] = 'Email';
  $_['entry_comment'] = 'Comment';
  $_['text_submit'] = 'Submit';
- $_['text_tags'] = 'Tags: ';
+ $_['text_tags'] = 'Thẻ: ';
  
  $_['text_like_this'] = 'Like This:';
   $_['entry_captcha'] = 'Captcha';
@@ -56,16 +56,16 @@ $_['text_postedby'] = 'Posted By';
 $_['bloglatest_heading_title'] = 'Latest';
 
 // Text
-$_['text_latest']  = 'Latest Blog'; 
-$_['text_mostviewed']  = 'Most Viewed'; 
+$_['text_latest']  = 'Tin Mới';
+$_['text_mostviewed']  = 'Tin Xem Nhiều';
 $_['text_featured']  = 'Featured'; 
 $_['text_bestseller']  = 'Best Seller'; 
 $_['entry_show_readmore'] = 'Show Readmore' ;
-$_['text_readmore'] = 'Read more';
+$_['text_readmore'] = 'Xem Thêm';
 
 
 // Page Blogs
-$_['text_blogs'] = 'Blogs';
+$_['text_blogs'] = 'Tin Tức';
 
 $_['text_error'] = 'Blogs not found!';
 
